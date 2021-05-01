e7fc1d386a3a6ac6dd0dc2e537432288c770ca37
CellBody <austintingwork@gmail.com>
2021-05-01 15:48:29 +0800

    Update test

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
 const assert = require('assert')
 
-describe('Array',function(){
-  describe('#indexOf()',function(){
-    it('Return -1 if element can not be searched',function(){
-      assert.equal(-1,[1,2,3].indexOf(0))
+let title = 'TODOList'
+
+describe('View',function(){
+  describe('Title',function(){
+    it('Title is ToDoList',function(){
+      assert.equal('ToDoList',title)
     })
   })
 })
